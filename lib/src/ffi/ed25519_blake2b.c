6dd2aee943a0a166f82510055eccfeb1ecf81c90
Brandon Berhent <bbedward@gmail.com>
2020-04-28 14:38:07 -0400

    More ffi progress

--- lib/src/ffi/ed25519_blake2b.c
+++ lib/src/ffi/ed25519_blake2b.c
@@ -2,7 +2,7 @@
 #include "ed25519-donna/ed25519-hash-custom.h"
 #include "ed25519-donna/ed25519.h"
 
-void ed25519_randombytes_unsafe(void *out, size_t outlen) {}
+//void ed25519_randombytes_unsafe(void *out, size_t outlen) {}
 
 blake2b_state b2b;
 
@@ -26,3 +26,30 @@ void ed25519_hash(uint8_t *out, uint8_t const *in, size_t inlen) {
   ed25519_hash_update(&ctx, in, inlen);
   ed25519_hash_final(&ctx, out);
 }
+
+unsigned char* dart_publickey(unsigned char *sk) {
+  static ed25519_public_key pk;
+
+  ed25519_publickey(sk, pk);
+  return &pk;
+}
+
+/*
+static PyObject *signature(PyObject *self, PyObject *args) {
+  const unsigned char *m, *randr, *sk, *pk;
+  int i, j, k, l;
+  ed25519_signature sig;
+
+  if (!PyArg_ParseTuple(args, "y#y#y#y#", &m, &i, &randr, &j, &sk, &k, &pk, &l))
+    return NULL;
+  ed25519_sign(m, i, randr, sk, pk, sig);
+  return Py_BuildValue("y#", &sig, 64);
+}
+
+static PyObject *checkvalid(PyObject *self, PyObject *args) {
+  const unsigned char *sig, *m, *pk;
+  int i, j, k;
+
+  if (!PyArg_ParseTuple(args, "y#y#y#", &sig, &i, &m, &j, &pk, &k)) return NULL;
+  return Py_BuildValue("i", ed25519_sign_open(m, j, pk, sig));
+}*/
--- lib/src/ffi/ed25519_blake2b.dart
+++ lib/src/ffi/ed25519_blake2b.dart
@@ -1,17 +1,40 @@
 import 'dart:ffi';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:ffi/ffi.dart';
+import 'package:path/path.dart' as p;
+
+// C publickey function - unsigned char* publickey(unsigned char *sk);
+typedef publickey_func = Pointer<Uint8> Function(Pointer<Uint8> sk);
+typedef Publickey = Pointer<Uint8> Function(Pointer<Uint8> sk);
 
 class Ed25519Blake2b {
   DynamicLibrary _dylib;
+  var pubkeyFunc;
 
   Ed25519Blake2b() {
-      String path = './libed25519_blake2b.so';
-      if (Platform.isMacOS) path = './libed25519_blake2b.dylib';
-      if (Platform.isWindows) path = r'libed25519_blake2b.dll';
+    String root = Directory.current.toString();
+    root = '/Users/beb279/repos/nanodart/lib/src/ffi';
+    String path = p.join(root, 'libed25519_blake2b.so');
+    if (Platform.isMacOS) path = p.join(root, 'libed25519_blake2b.dylib');
+    if (Platform.isWindows) path = p.join(root, 'libed25519_blake2b.dll');
 
-      // Open library
-      _dylib = DynamicLibrary.open(path);
+    // Open library
+    _dylib = DynamicLibrary.open(path);
+
+    // Publickey
+    final pkPointer = _dylib.lookup<NativeFunction<publickey_func>>('dart_publickey');
+    pubkeyFunc = pkPointer.asFunction<Publickey>();
+  }
+
+  Uint8List getPubkey(Uint8List secretKey) {
+    final pointer = allocate<Uint8>(count: secretKey.length);
+    for(var i = 0; i < secretKey.length; i++){
+      pointer[secretKey.length] = secretKey[i];
+    }
+    Pointer<Uint8> result = pubkeyFunc(pointer);
+    free(pointer);
+    return result.asTypedList(secretKey.length);
   }
 }
